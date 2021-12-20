726662540497c6c6b09a2bcc934a5bceb8da9e89
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-12-21 08:49:17 +0900

    🚑️: tsc型検証エラーを解消

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" onClick={hoge}>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
         <p>Verify GitHub Actions on pull request</p>
